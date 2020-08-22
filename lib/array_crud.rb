82a33e6b825de4508cde7633288d63546b8e21ad
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-22 19:20:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,8 +24,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  index_number = ["wow"]
-  index_number.shift "wow"
+  index_number = "wow"
 end
 
 def retrieve_element_from_index(array, index_number)
