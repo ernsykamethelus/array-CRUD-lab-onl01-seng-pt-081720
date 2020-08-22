4ea2f91572a5bb5fa343bfc062b987ab4f905af2
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-22 18:07:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,8 @@ def add_element_to_end_of_array(array, element)
   
   
   def add_element_to_start_of_array(array, element)
-    
+    array= ["I", "am", "really", "learning"]
+    element
   end
   
 
