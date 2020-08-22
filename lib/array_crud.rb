bfeda6c72edd106f8f524bce5f0d8fc987ae4f23
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-22 18:02:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,8 +13,8 @@ def add_element_to_end_of_array(array, element)
   
   
   def add_element_to_start_of_array(array, element)
-    index_number = array
-    index_number.unshift ("wow")
+    colors= ["blue", "red", "yellow" "rose"]
+    colors.unshift ("wow")
   end
   
 
