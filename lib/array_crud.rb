5ad7b087d24e62a338e1239bc61a205ec0b125bb
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-22 16:49:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ def add_element_to_end_of_array(array, element)
   end
   
   
-  def add_element_to_start_of_array.first
+  def add_element_to_start_of_array(array, element)
     science = ["astrology", "numerology", "stars", "spiritualy"]
     science.unshift ("wow")
   end
