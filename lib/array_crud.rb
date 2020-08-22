ff64f91d04a57090497eed50a922a467aba8bb3b
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-22 17:10:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ def add_element_to_end_of_array(array, element)
 
 def add_element_to_start_of_array(array, element)
   science = ["astrology", "numerology", "stars", "spiritualy", "arrays"]
-  science.shift
+  science.shift "arrays!"
 end
 
 def remove_element_from_end_of_array(array)
