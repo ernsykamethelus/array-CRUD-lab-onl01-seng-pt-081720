89d25fd018476104799d48e99188ea0ecbba6112
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-22 17:52:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,12 +3,12 @@ def create_an_empty_array
 end
 
 def create_an_array
- array = ["1", "2", "3", "4"]
+ index_number = ["1", "2", "3", "4"]
 end
 
 def add_element_to_end_of_array(array, element)
-    array = ["1", "2", "3", "4"]
-    element << "arrays!"
+    index_number = ["1", "2", "3", "4"]
+    index_number << "arrays!"
   end
   
   
