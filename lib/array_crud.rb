f242fda43e108ec1a4bddc0f5e9b86335e40b5a3
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-22 19:58:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  spec_helper = ["wow", "I", "am", "really", "learning", "arrays!"]
+  index_number = ["wow", "I", "am", "really", "learning", "arrays!"]
   index_number "2"
 end
 
