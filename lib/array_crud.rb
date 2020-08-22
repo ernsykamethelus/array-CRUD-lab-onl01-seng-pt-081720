c57e9044e6e10345906f8b53843a3bf41e41f0a6
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-22 19:40:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,31 +4,31 @@ def create_an_empty_array
 end
 
 def create_an_array
- spec_helper = ["1", "2", "3", "4"]
+ spec_helper = ["wow", "I", "am", "really", "learning"]
 end
 
 def add_element_to_end_of_array(array, element)
-    index_number = ["1", "2", "3", "4"]
-    index_number << "arrays!"
+    spec_helper = ["wow", "I", "am", "really", "learning"]
+    spec_helper << "arrays!"
   end
   
   
   def add_element_to_start_of_array(array, element)
-    index_number = ["1", "2", "3", "4"]
-    index_number.unshift ("wow")
+    spec_helper = ["wow", "I", "am", "really", "learning"]
+    spec_helper.unshift ("wow")
   end
   
 
 def remove_element_from_end_of_array(array)
-  index_number = "arrays!"
+  spec_helper = "arrays!"
 end
 
 def remove_element_from_start_of_array(array)
-  index_number = "wow"
+  spec_helper = "wow"
 end
 
 def retrieve_element_from_index(array, index_number)
-  index_number = ["1", "2", "3", "4"]
+  spec_helper = ["1", "2", "3", "4"]
   index_number =
 end
 
