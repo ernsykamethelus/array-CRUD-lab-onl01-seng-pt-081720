5ffae07905269ec4861c932cb93efe1372a3346e
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-22 19:19:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,7 +21,6 @@ def add_element_to_end_of_array(array, element)
 
 def remove_element_from_end_of_array(array)
   index_number = "arrays!"
-  index_number.pop "arrays!"
 end
 
 def remove_element_from_start_of_array(array)
